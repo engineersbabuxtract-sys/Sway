5d7d6877ceaff22793bbf39efa7f20a121acd8ab
engineersbabuxtract-sys <engineersbabuxtract@gmail.com>
2026-07-22 10:46:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ PORT = int(os.getenv("PORT", "8080"))
 API_BASE = "https://gdgoenkaratia.com/api"
 USER_ID = os.getenv("USER_ID", "")
 
-SMTP_EMAIL = os.getenv("SMTP_Server_Hostname", "")
-SMTP_PASSWORD = os.getenv("Port", "")
+SMTP_EMAIL = os.getenv("SMTP_EMAIL", "")
+SMTP_PASSWORD = os.getenv("SMTP_PASSWORD", "")
 
 def get_html():
     html_path = Path(__file__).parent / "index.html"
